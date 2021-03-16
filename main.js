f000184b0a6de24bb1d8533799d2f80c88708486
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-16 12:37:45 -0400

    Solves warmups and event listeners, still needs stretch goals

--- main.js
+++ main.js
@@ -1,51 +1,51 @@
+// Number 1
 const messWithMe = document.querySelector('span.mess-with-me');
-
-const messWithPara = document.querySelector('p.mess-with-me');
-
 messWithMe.style.fontSize = '40px';
+
+// Number 2
+const messWithPara = document.querySelector('p.mess-with-me');
 messWithPara.style.backgroundColor = 'green';
 
+// Number 3
 const hideMe = document.querySelector("#hide-me-area");
-
 hideMe.style.display = "none";
 
+// Number 4
 const triceratops = document.querySelector('#triceratops');
-
 triceratops.style.width = "324px";
 
+// Event Listener 1
 function changeColor() {
     messWithMe.style.color ='orange';
 }
-
 messWithMe.addEventListener('click', changeColor)
 
+// Event Listener 2
 function redBorder() {
     triceratops.style.border = "1px solid red"
 }
-
 triceratops.addEventListener('click', redBorder)
 
+// Event Listener 3
 const feathers = document.querySelector('#feathers');
-
 function transparent() {
     feathers.style.opacity = '50%';
 }
+feathers.addEventListener('click', transparent);
 
-feathers.addEventListener('click', transparent)
-
-const biggify = document.querySelector('#biggify');
-
-function growImg() {
-    biggify.style.width = "200px";
-}
-
-biggify.addEventListener("mouseover", growImg)
-
- 
- function changeBackground() {
+// Event Listener 4
+function changeBackground() {
     document.querySelector('#row').style.backgroundColor = "black";
 }
 
 const button = document.querySelector('#toggle')
 
 button.addEventListener('click', changeBackground)
+
+
+// Event Listener 5
+const biggify = document.querySelector('#biggify');
+function growImg() {
+    biggify.style.width = "200px";
+}
+biggify.addEventListener("mouseover", growImg)
