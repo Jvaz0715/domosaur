d80cfd08d72c1fe06364df92ebafafe007e2914d
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-16 11:22:24 -0400

    Hides the second dinosaur

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const messWithMe = document.querySelector('.mess-with-me');
 
 messWithMe.style.fontSize = '40px';
+messWithMe.style.backgroundColor = 'green';
+
+const hideMe = document.querySelector('#hide-me-area');
+
+hideMe.style.display = "none";
